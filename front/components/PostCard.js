815b5644ef59c51683aa2cf0ed78b1775c8e09a0
YOOJS1205 <junsang8921@gmail.com>
2022-09-08 16:26:10 +0900

    Feat: 게시글 구현

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,5 +1,71 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { Button, Card, Popover, Avatar } from "antd";
+import {
+  RetweetOutlined,
+  HeartOutlined,
+  HeartTwoTone,
+  MessageOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
+import { useSelector } from "react-redux";
 
-export default function PostCard() {
-  return <div>PostCard</div>;
+import PostImages from "./PostImages";
+
+export default function PostCard({ post }) {
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+  const id = useSelector((state) => state.user.me?.id);
+
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, []);
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened((prev) => !prev);
+  }, []);
+  return (
+    <div style={{ marginBottom: 20 }}>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          <RetweetOutlined key="retweet" />,
+          liked ? (
+            <HeartTwoTone
+              twoToneColor="#eb2f96"
+              key="heart"
+              onClick={onToggleLike}
+            />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
+          <MessageOutlined key="comment" onClick={onToggleComment} />,
+          <Popover
+            key="more"
+            content={
+              <Button.Group>
+                {id && post.User.id === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button type="danger">삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+          description={post.content}
+        />
+      </Card>
+      {commentFormOpened && <div>댓글 부분</div>}
+      {/* <CommentForm /> */}
+      {/* <Comments /> */}
+    </div>
+  );
 }
---
+++ front/components/PostImages.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PostImages({ images }) {
+  return <div>구현중...</div>;
+}
