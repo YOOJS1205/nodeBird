3ae96cb500b0ad1e1af7def8fc201cba89a9b53d
YOOJS1205 <junsang8921@gmail.com>
2022-09-09 16:01:15 +0900

    Feat: redux-saga 적용

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,19 +1,22 @@
 import { createWrapper } from "next-redux-wrapper";
 import { createStore, applyMiddleware, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunkMiddleware from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "../reducers";
 
 const loggerMiddleware =
   ({ dispatch, getState }) =>
   (next) =>
   (action) => {
+    const state = getState();
+    console.log(state);
     console.log(action);
     return next(action);
   };
 
 const configureStore = () => {
-  const middlewares = [thunkMiddleware, loggerMiddleware];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
