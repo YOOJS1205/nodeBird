5c3bbcdbddaf2869122a5a9b104786d24091e813
YOOJS1205 <junsang8921@gmail.com>
2022-09-12 19:17:55 +0900

    Chore: sequelize, mysql 추가

---
+++ back/routes/post.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  res.send({ id: 1, content: "hello" });
+});
+
+router.delete("/", (req, res) => {
+  res.send({ id: 1 });
+});
+
+module.exports = router;
