a54f537750d79bceb36b6ba29c36187c98df3db5
YOOJS1205 <junsang8921@gmail.com>
2022-09-26 15:28:14 +0900

    Feat: 메인 페이지 ssr 적용

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { END } from "redux-saga";
 import { LOAD_POSTS_REQUEST } from "../reducers/post";
 import { LOAD_MY_INFO_REQUEST } from "../reducers/user";
+import wrapper from "../store/configureStore";
 
 import AppLayout from "../components/AppLayout";
 import PostForm from "../components/PostForm";
@@ -18,15 +20,6 @@ const Home = () => {
       alert(retweetError);
     }
   }, [retweetError]);
-
-  useEffect(() => {
-    dispatch({
-      type: LOAD_MY_INFO_REQUEST,
-    });
-    dispatch({
-      type: LOAD_POSTS_REQUEST,
-    });
-  }, []);
 
   useEffect(() => {
     function onScroll() {
@@ -58,5 +51,22 @@ const Home = () => {
     </AppLayout>
   );
 };
+
+// 해당 부분이 Home 페이지보다 먼저 실행
+// 화면을 그리기 전에 서버에서 먼저 실행
+// 데이터를 채우고 화면에 렌더링을 시킬 수 있다.
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    console.log(store);
+    store.dispatch({
+      type: LOAD_MY_INFO_REQUEST,
+    });
+    store.dispatch({
+      type: LOAD_POSTS_REQUEST,
+    });
+    store.dispatch(END);
+    await store.sagaTask.toPromise();
+  }
+);
 
 export default Home;
--- front/reducers/index.js
+++ front/reducers/index.js
@@ -5,22 +5,38 @@ import post from "./post";
 
 // 이전상태와 액션을 통해서 다음 상태를 만드는 함수
 // combineReducers가 각각의 initial state를 알아서 넣어줌
-const rootReducer = combineReducers({
-  // redux ssr을 위한 hydrate 때문에 index reducer 추가
-  index: (state = {}, action) => {
-    switch (action.type) {
-      case HYDRATE:
-        console.log("HYDRATE", action);
-        return {
-          ...state,
-          ...action.payload,
-        };
-      default:
-        return state;
+// const rootReducer = combineReducers({
+//   // redux ssr을 위한 hydrate 때문에 index reducer 추가
+//   index: (state = {}, action) => {
+//     switch (action.type) {
+//       case HYDRATE:
+//         console.log("HYDRATE", action);
+//         return {
+//           ...state,
+//           ...action.payload,
+//         };
+//       default:
+//         return state;
+//     }
+//   },
+//   user,
+//   post,
+// });
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case HYDRATE: {
+      console.log("HYDRATE", action);
+      return action.payload;
     }
-  },
-  user,
-  post,
-});
+    default: {
+      const combinedReducer = combineReducers({
+        user,
+        post,
+      });
+      return combinedReducer(state, action);
+    }
+  }
+};
 
 export default rootReducer;
